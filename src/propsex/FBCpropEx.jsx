ee23c138ba414af0b4ddba81e14e73a43c1cca3c
noshitha613 <noshithanoshitha@gmail.com>
2025-02-06 14:08:19 +0530

    day

--- src/propsex/FBCpropEx.jsx
+++ src/propsex/FBCpropEx.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+// import React from 'react'
 
-const FBCpropEx = (props) => {
-    console.log(props);
-    if(props.isMarried== true){
-        return (
-            <div>
-                <h1>{props.username} is a married person</h1>
-                {
-                    props.hobbies.map(hobby=>{
-                        return <li>{hobby}</li>
-                    })
-                }
-            </div>
-        )
-    }else{
-        return <h1>Not married</h1>
-    }
-    }
-export default FBCpropEx
+// const FBCpropEx = (props) => {
+//     console.log(props);
+//     if(props.isMarried== true){
+//         return (
+//             <div>
+//                 <h1>{props.username} is a married person</h1>
+//                 {
+//                     props.hobbies.map(hobby=>{
+//                         return <li>{hobby}</li>
+//                     })
+//                 }
+//             </div>
+//         )
+//     }else{
+//         return <h1>Not married</h1>
+//     }
+//     }
+// export default FBCpropEx
