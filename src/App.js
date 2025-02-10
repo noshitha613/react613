82fa719d8ab096be27e6c0de16b16cd2364edf52
noshitha613 <noshithanoshitha@gmail.com>
2025-02-10 12:58:09 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@
 // }
 
 // export default App
-import React from 'react'
-import CBCAssignment from './stateexample/CBCAssignment'
-const App = () => {
+// import React from 'react'
+// import CBCAssignment from './stateexample/CBCAssignment'
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <CBCAssignment/> */}
+//     </div>
+//   )
+// }
+
+// export default App
+import React from 'react';
+import UseEffectEx from './hookexample/UseEffectEx';
+
+function App() {
   return (
-    <div>
-      {/* <CBCAssignment/> */}
+    <div className="App">
+      <h1>React UseEffect Example</h1>
+      <UseEffectEx />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
